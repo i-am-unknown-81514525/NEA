9934d7f4a54e854c35838a8ca1ab0b0726b4d036
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2025-06-19 12:53:19 +0000

    Update ui submodule - fixedgit add .!

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+rm -r build || true
 msbuild || true
 cd ui/stdin_handler 
 cargo build --release || true
@@ -6,3 +7,4 @@ mkdir build
 cp NEA/bin/Debug/* build || true
 cp ui/bin/Debug/* build || true
 cp ui/stdin_handler/target/release/* build || true
+touch build/read-log
