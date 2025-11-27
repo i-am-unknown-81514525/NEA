b38d8dbaa5f4e76bbe26821fb76b39d58c090272
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2025-11-27 13:38:43 +0000

    Add Sample Assessment test

--- NEA.Tests/math/SimplexTest.cs
+++ NEA.Tests/math/SimplexTest.cs
@@ -2,6 +2,7 @@ using NEA.math;
 using NUnit.Framework;
 using ui.math;
 using math_parser;
+using System.Collections.Generic;
 
 namespace NEA.Tests.math
 {
@@ -115,6 +116,52 @@ namespace NEA.Tests.math
                         }
                     )
                 );
+            }
+        }
+
+        [Test]
+        // Sample Assessment Material A Level OCR MEI B Further Math Modelling with Algorithms Q5/Q5(ii)
+        public void LpModelToTableauTest2()
+        {
+            string question = @"MAX 1/3x + 1/2y
+ST
+x + 2y <= 9
+2x + 3y <= 14
+2x + y <= 10
+END";
+            SimplexInterationRunner runner = ToSimplexRunner.Translate(question);
+            using (Assert.EnterMultipleScope())
+            {
+
+                Assert.That(
+                    runner.Expressions,
+                    Is.EquivalentTo(
+                        new Fraction[,]
+                        {
+                            {1, 0, 0, 0},
+                            {0, 0, 1, 0},
+                            {0, 0, 0, 1},
+                            {0, 1, 0, 0},
+                            {new Fraction(1, 6), new Fraction(-3, 4), new Fraction(-1, 4), new Fraction(1, 2)},
+                            {0, new Fraction(1, 4), new Fraction(3, 4), new Fraction(-1, 2)},
+                            {new Fraction(7, 3), 1, 4, 2}
+                        }
+                    )
+                );
+
+                Assert.That(
+                    runner.Vars,
+                    Is.EquivalentTo(new [] {"P", "x", "y", "s_1", "s_2", "s_3"})
+                );
+
+                
+
+                Dictionary<string, Fraction> result = runner.Resolve();
+
+                Assert.That(result, Does.ContainKey("P").WithValue(new Fraction(7, 4)));
+                Assert.That(result, Does.ContainKey("x").WithValue((Fraction)4));
+                Assert.That(result, Does.ContainKey("y").WithValue((Fraction)2));
+                Assert.That(result, Does.ContainKey("s_1").WithValue((Fraction)1));
             }
         }
     }
